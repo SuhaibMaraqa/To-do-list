c964a7121b15d6c3d042b78d9a3f2b663f730e52
Sting1X <suhaibmaraqaapple@gmail.com>
2021-09-11 15:26:36 +0300

    Design done

--- app.js
+++ app.js
@@ -7,26 +7,28 @@ app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: true }))
 
-var items=[];
+var items=["A", "B", "C"];
 
-app.get("/", (req, res) => {
-     var options = {
-          weekday: 'long',
-          month: 'long',
-          day: 'numeric'
+
+app.get("/", (req, res) => {    
+     var options={
+          weekday: "long",
+          month: "long",
+          day: "numeric"
      };
      var today = new Date();
      var day = today.toLocaleDateString("en-US", options);
-
      res.render("list", {today: day, newItems: items});
+
 })
 
-app.post("/", (req, res) => {
-     item = req.body.text; 
+app.post("/", (req, res) =>{
+     item = req.body.text;
      items.push(item);
      res.redirect("/");
-})
+});
 
 app.listen(3000, () =>{
      console.log("Server started on port 3000.")
 })
+
