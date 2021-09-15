4f24b96c7b25440918dbaae4c139704a85844921
Sting1X <suhaibmaraqaapple@gmail.com>
2021-09-15 21:12:42 +0300

    finish gitignore

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: true }))
 
-mongoose.connect('mongodb://localhost:27017/todolistDBB');
+mongoose.connect('mongodb+srv://admin-suhaib:WiN7RAwtZbt3cnr8@cluster0.uc2sr.mongodb.net/todolistDBB');
 
 const itemSchema = new mongoose.Schema({
      name: String
@@ -130,7 +130,12 @@ app.post("/delete", (req, res)=>{
      
 });
 
-app.listen(3000, () =>{
-     console.log("Server started on port 3000.")
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, () =>{
+     console.log("Server has started successfully.")
 })
 
